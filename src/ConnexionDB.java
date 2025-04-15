d7f6b4657f2b383f75733ac2a9b743f0d8edbfe4
Chaymae Bayousfi <cbayousf@e3r5p2.1337.ma>
2025-04-15 19:01:30 +0100

    update

---
+++ src/ConnexionDB.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnexionDB {
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Charger le driver
+            Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/centre_formation", // URL
+                "root",       // utilisateur MySQL
+                ""            // mot de passe (vide si tu n’as rien mis)
+            );
+            System.out.println("✅ Connexion réussie à la base de données !");
+            return conn;
+        } catch (ClassNotFoundException e) {
+            System.out.println("❌ Driver JDBC non trouvé !");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("❌ Erreur de connexion !");
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        ConnexionDB.getConnection(); // Tester la connexion
+    }
 }
