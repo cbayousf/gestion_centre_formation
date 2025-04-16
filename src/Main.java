b5e5b5d29451cbee0a7b937773305200db358365
Chaymae Bayousfi <cbayousf@e3r6p4.1337.ma>
2025-04-16 20:39:07 +0100

    connexion reussie

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import conn.ConnexionDB;
+
 public class Main {
     public static void main(String[] args) {
         ConnexionDB.getConnection(); // Tester la connexion
--- src/conn/ConnexionDB.java
+++ src/conn/ConnexionDB.java
@@ -1,3 +1,4 @@
+package conn;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
